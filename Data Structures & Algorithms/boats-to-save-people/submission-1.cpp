41050a8645fda5e95a7e1225e2bbe49d78197091
rajat09378-byte <rajat09378@gmail.com>
2026-09-18 17:43:51 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());  int count=0;
+        int i=0;
+        int j=people.size()-1;
+        while(i<=j){ if(i==j){
+           
+        }
+            if(people[i]+people[j]<= limit){
+                i++;
+                j--;
+                count ++;
+
+            }
+            else {
+            j--;
+            count++;
+            }
+        }
+        return count;
+    }
+};
